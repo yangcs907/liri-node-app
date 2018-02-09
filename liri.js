77f2654517523244355a6b4e3f242d263f499db9
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-02-09 10:58:20 -0500

    updates to tweet function

--- liri.js
+++ liri.js
@@ -24,7 +24,7 @@ var Twitter = require('twitter');
 
 var client = new Twitter(keys.twitter);
 
-var params = {screen_name: 'nodejs'};
+var params = {screen_name: 'csyazn907'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
